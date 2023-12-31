c73990f984bf3191e6b5730a47544b58bf9ca33c
moaz <moazemails@gmail.com>
2023-12-31 18:25:40 +0200

    update OrderingShipmentState

--- demo/demo/src/main/java/com/example/demo/Service/OrderShipmentState.java
+++ demo/demo/src/main/java/com/example/demo/Service/OrderShipmentState.java
@@ -1,6 +1,7 @@
 package com.example.demo.Service;
 import com.example.demo.Model.CustomerAccount;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 public class OrderShipmentState {
     private boolean orderShipmentState = false;
     private OrderList orderList = OrderList.getInstant();
@@ -19,12 +20,17 @@ public class OrderShipmentState {
             account.setBalance(account.getBalance() - orderList.ReturnOrder(username,orderID).getShipping());
         }
     }
-    public void OrderShipmentStateCancellation(String username, int orderID){
+    public void OrderShipmentStateCancellation(String username, int orderID, LocalDateTime shipmentStartTime, int allowedCancellationDurationInHours){
         CustomerAccount account = new CustomerAccount();
         account.getAccount(username);
         if(!orderList.ReturnOrder(username,orderID).getOrderShipmentState()){
             return;
         }else{
+            LocalDateTime cancellationEndTime = shipmentStartTime.plusHours(allowedCancellationDurationInHours);
+            LocalDateTime currentTime = LocalDateTime.now();
+            if (currentTime.isAfter(cancellationEndTime)) {
+                return;
+            }
             orderList.ReturnOrder(username,orderID).setOrderShipmentState(false);
             orderShipmentState = false;
             account.setBalance(account.getBalance() - orderList.ReturnOrder(username,orderID).getShipping());
